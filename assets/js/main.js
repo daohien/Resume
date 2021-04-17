6b2ad130c6bcbdf85b2849b0c4095eb2ed630468
dao hien <hiendao07062000@gmail.com>
2021-04-17 11:08:27 +0700

    Resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,24 @@ window.onload = function() {
                 header.classList.remove("active");
             }
         })
+        var navbar = document.querySelector(".navbar");
+        var menu = document.querySelector(".menu");
+        var check = "checkFirst";
+        navbar.addEventListener("click", ()=>{
+            if(check === "checkFirst") {
+                check = "checkSecond";
+                navbar.classList.add("active");
+                menu.classList.add("active");
+            } else if(check === "checkSecond"){
+                check = "checkFirst";
+                navbar.classList.remove("active");
+                menu.classList.remove("active")
+            }
+        })
     }
     headers()
 
     new WOW().init();
+
+
 }
